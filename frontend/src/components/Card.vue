6650cd8964cee87231b181793b38f9ca94103fed
Kninja99 <kcdamschen@yahoo.com>
2022-11-04 19:21:30 -0700

    Card component init
    * added a global style sheet
    * template for card component created (Need to add props)
    * added a card component to admin page so we can see the style

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="card">
+        <img class="card-logo" src="../assets/imgs/room-icon.svg" alt="logo">
+        <button class="card-btn">Click Me</button>
+    </div>
+</template>
--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,5 +1,17 @@
+<script>
+import Card from "../components/Card.vue"
+export default {
+    components: {
+        Card
+    }
+}
+</script>
+
+
+
 <template>
     <div class="admin-home">
         <h1>Admin home page</h1>
+        <Card />
     </div>
 </template>
