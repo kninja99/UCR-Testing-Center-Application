eb3aeea0acd4fcb86096d376b750ecfe292aa8eb
Kninja99 <kcdamschen@yahoo.com>
2022-11-08 20:57:48 -0800

    2nd card addition
    * added another card component to admin portal
    * added the img logo to the card component

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -13,5 +13,6 @@
     <div class="admin-home">
         <h1>Admin home page</h1>
         <Card img-name="room-icon.svg" button-header="View Rooms"/>
+        <Card img-name="email-icon.svg" button-header="View Inbox"/>
     </div>
 </template>
