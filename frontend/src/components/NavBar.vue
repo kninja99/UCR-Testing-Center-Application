7187d7384adebbbed0c333ff822ac0fc8e06217d
Kninja99 <kcdamschen@yahoo.com>
2022-11-13 17:47:11 -0800

    navElement comp made

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,28 +1,19 @@
+<script>
+    import NavElement from './NavElement.vue';
+    export default {
+        components: {
+         NavElement
+        }
+    }
+</script>
+
 <template>
     <div class="nav-bar">
-        <a href="/admin" class= nav-comp>
-            <img src="../assets/imgs/dashBoard-icon.svg" alt="dashlog">
-            <h3>Dashboard</h3>
-        </a>
-        <a href="admin/rooms" class= nav-comp>
-            <img src="../assets/imgs/roomNav-icon.svg" alt="dashlog">
-            <h3>Rooms</h3>
-        </a>
-        <a href="admin/inbox" class= nav-comp>
-            <img src="../assets/imgs/mailNav-icon.svg" alt="dashlog">
-            <h3>Inbox</h3>
-        </a>
-        <a href="admin/settings" class= nav-comp>
-            <img src="../assets/imgs/settings-icon.svg" alt="dashlog">
-            <h3>Settings</h3>
-        </a>
-        <a href="admin/account" class= nav-comp>
-            <img src="../assets/imgs/profile-icon.svg" alt="dashlog">
-            <h3>My Account</h3>
-        </a>
-        <a href="/login" class= nav-comp>
-            <img src="../assets/imgs/logout-icon.svg" alt="dashlog">
-            <h3>Logout</h3>
-        </a>
+        <NavElement navTitle = "Dashboard" imgName = "dashBoard-icon.svg" route = "/admin"/>
+        <NavElement navTitle = "Rooms" imgName = "roomNav-icon.svg" route = "admin/rooms"/>
+        <NavElement navTitle = "Inbox" imgName = "mailNav-icon.svg" route = "admin/inbox"/>
+        <NavElement navTitle = "Settings" imgName = "settings-icon.svg" route = "admin/settings"/>
+        <NavElement navTitle = "My Account" imgName = "profile-icon.svg" route = "admin/account"/>
+        <NavElement navTitle = "Logout" imgName = "logout-icon.svg" route = "/login"/>
     </div>
 </template>
---
+++ frontend/src/components/NavElement.vue
@@ -0,0 +1,16 @@
+<script>
+    export default {
+        props: {
+            navTitle: String,
+            imgName: String,
+            route: String
+        }
+    }   
+</script>
+
+<template>
+    <a :href="route" class= nav-comp>
+        <img :src="require(`../assets/imgs/${imgName}`)" :alt="imgName">
+        <h3>{{navTitle}}</h3>
+    </a>
+</template>
