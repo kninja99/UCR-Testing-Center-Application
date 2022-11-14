8349190f31c6e838c4e37070ab1ca6ca12947d25
Kninja99 <kcdamschen@yahoo.com>
2022-11-13 17:07:39 -0800

    nav bar hard code update
    * added a nav bar thats hard coded
    * no rerouting done yet

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,8 +1,28 @@
 <template>
     <div class="nav-bar">
         <div class= nav-comp>
-            <img src="" alt="dashlog">
+            <img src="../assets/imgs/dashBoard-icon.svg" alt="dashlog">
             <h3>Dashboard</h3>
+        </div>
+        <div class= nav-comp>
+            <img src="../assets/imgs/roomNav-icon.svg" alt="dashlog">
+            <h3>Rooms</h3>
+        </div>
+        <div class= nav-comp>
+            <img src="../assets/imgs/mailNav-icon.svg" alt="dashlog">
+            <h3>Inbox</h3>
+        </div>
+        <div class= nav-comp>
+            <img src="../assets/imgs/settings-icon.svg" alt="dashlog">
+            <h3>Settings</h3>
+        </div>
+        <div class= nav-comp>
+            <img src="../assets/imgs/profile-icon.svg" alt="dashlog">
+            <h3>My Account</h3>
+        </div>
+        <div class= nav-comp>
+            <img src="../assets/imgs/logout-icon.svg" alt="dashlog">
+            <h3>Logout</h3>
         </div>
     </div>
 </template>
