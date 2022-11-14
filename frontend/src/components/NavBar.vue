443ae796eddb77c7763c84a3b3160d458f9adcd9
Kninja99 <kcdamschen@yahoo.com>
2022-11-13 17:22:55 -0800

    Nav Comp now routeing
    * now nav comp will route to dummy links

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,28 +1,28 @@
 <template>
     <div class="nav-bar">
-        <div class= nav-comp>
+        <a href="/admin" class= nav-comp>
             <img src="../assets/imgs/dashBoard-icon.svg" alt="dashlog">
             <h3>Dashboard</h3>
-        </div>
-        <div class= nav-comp>
+        </a>
+        <a href="admin/rooms" class= nav-comp>
             <img src="../assets/imgs/roomNav-icon.svg" alt="dashlog">
             <h3>Rooms</h3>
-        </div>
-        <div class= nav-comp>
+        </a>
+        <a href="admin/inbox" class= nav-comp>
             <img src="../assets/imgs/mailNav-icon.svg" alt="dashlog">
             <h3>Inbox</h3>
-        </div>
-        <div class= nav-comp>
+        </a>
+        <a href="admin/settings" class= nav-comp>
             <img src="../assets/imgs/settings-icon.svg" alt="dashlog">
             <h3>Settings</h3>
-        </div>
-        <div class= nav-comp>
+        </a>
+        <a href="admin/account" class= nav-comp>
             <img src="../assets/imgs/profile-icon.svg" alt="dashlog">
             <h3>My Account</h3>
-        </div>
-        <div class= nav-comp>
+        </a>
+        <a href="/login" class= nav-comp>
             <img src="../assets/imgs/logout-icon.svg" alt="dashlog">
             <h3>Logout</h3>
-        </div>
+        </a>
     </div>
 </template>
