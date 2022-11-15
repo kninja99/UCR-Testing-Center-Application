0151ce5b758e5d29d3fed45d27a1d7b3105d1f7d
katdario <62965281+katdario@users.noreply.github.com>
2022-11-14 21:40:52 -0800

    Added login background and header

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,5 +1,19 @@
 <template>
     <div class="login">
-        <h1>Login Page</h1>
+        <h1 style="color:white;">Welcome to the UCR Testing Center</h1>
+        
     </div>
 </template>
+
+<style>
+body {
+  position: relative;
+  background-image: url('../assets/imgs/loginBackground.svg') ;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
+
+
+
