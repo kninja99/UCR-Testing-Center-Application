d7e32b3ebca451d10b4568f6eda0014c35432883
katdario <62965281+katdario@users.noreply.github.com>
2022-11-18 10:21:45 -0800

    Updated naming for consistency

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="login">
-        <h1 class="loginWelcome"><b>Welcome to the UCR Testing Center</b></h1>
-        <p class="loginTrouble">Trouble Connecting?<br>Contact us at: sendhelp@ucr.edu</p>
+        <h1 class="login-welcome"><b>Welcome to the UCR Testing Center</b></h1>
+        <p class="login-trouble">Trouble Connecting?<br>Contact us at: sendhelp@ucr.edu</p>
     </div>
 </template>
 
