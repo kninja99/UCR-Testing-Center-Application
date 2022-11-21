d4e522f1fb728c58ebe3a0b84094a67c20cde074
Kninja99 <kcdamschen@yahoo.com>
2022-11-21 09:59:53 -0800

    init process of adding corner styles

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,11 +1,12 @@
 <script>
-    import Card from "../components/Card.vue"
-    import NavBar from "../components/NavBar.vue"
-    export default {
-        components: {
-         Card, NavBar
-        }
+import Card from "../components/Card.vue"
+import NavBar from "../components/NavBar.vue"
+import CornerStyle from "../components/CornerStyle.vue";
+export default {
+    components: {
+        Card, NavBar, CornerStyle
     }
+}
 </script>
 
 
@@ -13,19 +14,22 @@
 <template>
     <div class="admin-home">
         <!-- Temp header for styling reasons -->
-        <header><h1>Admin Home Page</h1></header>
+        <header>
+            <h1>Admin Home Page</h1>
+        </header>
         <!-- nav-elements is an array of objects that map nav elements -->
         <!-- object {title:Sting , img:String , route:String} -->
-        <NavBar :nav-elements = "[
-            {title: 'Dashboard', img: 'dashBoard-icon.svg', route:'/admin'},
-            {title: 'Rooms', img: 'roomNav-icon.svg', route:'admin/rooms'},
-            {title: 'Inbox', img: 'mailNav-icon.svg', route:'admin/inbox'},
-            {title: 'Settings', img: 'settings-icon.svg', route:'admin/settings'},
-            {title: 'My Account', img: 'profile-icon.svg', route:'admin/account'},
-            {title: 'Logout', img: 'logout-icon.svg', route:'/login'}]" />
+        <NavBar :nav-elements="[
+        { title: 'Dashboard', img: 'dashBoard-icon.svg', route: '/admin' },
+        { title: 'Rooms', img: 'roomNav-icon.svg', route: 'admin/rooms' },
+        { title: 'Inbox', img: 'mailNav-icon.svg', route: 'admin/inbox' },
+        { title: 'Settings', img: 'settings-icon.svg', route: 'admin/settings' },
+        { title: 'My Account', img: 'profile-icon.svg', route: 'admin/account' },
+        { title: 'Logout', img: 'logout-icon.svg', route: '/login' }]" />
         <div class="card-container">
-            <Card img-name="room-icon.svg" button-header="View Rooms"/>
-            <Card img-name="email-icon.svg" button-header="View Inbox"/>
+            <Card img-name="room-icon.svg" button-header="View Rooms" />
+            <Card img-name="email-icon.svg" button-header="View Inbox" />
         </div>
+        <CornerStyle />
     </div>
 </template>
