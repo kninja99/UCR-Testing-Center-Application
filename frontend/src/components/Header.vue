bd89f4ed902c77519a15f9d2f868a0565b3f8924
GregariousG <91446233+GregariousG@users.noreply.github.com>
2022-11-21 12:12:03 -0800

    added missing files

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        props: {
+            title: String
+        }
+    }
+</script>
+
+
+
+<template>
+    <header class="header">
+        <div class="UCR-logo">
+            <img src="../assets/imgs/UCRiverside-icon.svg">
+        </div>
+        <h1 class = theHeader>{{title}}</h1>
+    </header>
+</template>
