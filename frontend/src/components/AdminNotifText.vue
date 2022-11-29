b99bd81c9d4376e8b1e1628a99e82f3ec353f088
katdario <62965281+katdario@users.noreply.github.com>
2022-11-28 16:00:55 -0800

    created 3 text columns

---
+++ frontend/src/components/AdminNotifText.vue
@@ -0,0 +1,52 @@
+<script>
+    export default {
+        props: {
+            profName: {
+                type:String,
+                required: true
+            },
+            className: {
+                type:String,
+                required: true
+            },
+            roomNum: {
+                type:String,
+                required: true
+            }
+        }
+    }   
+</script>
+
+<template>
+    <!-- 
+            schedDateFrom: {
+                type:Date,
+                required: true
+            },
+            schedDateTo: {
+                type:Date,
+                required: true
+            },
+            schedTimeRange: {
+                type:TimeRanges,
+                required: true
+            }
+
+        <a :href="route" class= nav-comp>
+        <img :src="require(`../assets/imgs/${imgName}`)" :alt="imgName">
+        <h3>{{navTitle}}</h3>
+    </a> -->
+    <div class="admin-notif-text">
+        <div class="prof-column">
+            <p>Prof: {{profName}} <br>{{className}}</p>
+        </div>
+
+        <div class="room-column">
+            <p>Room: {{roomNum}}</p>
+        </div>
+        
+        <div class="sched-column">
+            <p>Schedule</p>
+        </div>
+    </div> 
+</template>
--- frontend/src/views/AdminInbox.vue
+++ frontend/src/views/AdminInbox.vue
@@ -1,3 +1,29 @@
+<script>
+import Card from "../components/Card.vue"
+import NavBar from "../components/NavBar.vue"
+import Header from "../components/Header.vue"
+import CornerStyle from "../components/CornerStyle.vue"
+import AdminNotif from "../components/AdminNotifText.vue"
+
+export default {
+    components: {
+        Card, NavBar, Header, CornerStyle, AdminNotif
+    }
+}
+</script>
+
 <template>
-    <h1>Admin Inbox</h1>
+    <div class="admin-inbox">
+        <Header title="Admin Inbox"/>
+        <NavBar :nav-elements="[
+            { title: 'Dashboard', img: 'dashBoard-icon.svg', route: '/admin' },
+            { title: 'Rooms', img: 'roomNav-icon.svg', route: 'admin/rooms' },
+            { title: 'Inbox', img: 'mailNav-icon.svg', route: 'admin/inbox' },
+            { title: 'Settings', img: 'settings-icon.svg', route: 'admin/settings' },
+            { title: 'My Account', img: 'profile-icon.svg', route: 'admin/account' },
+            { title: 'Logout', img: 'logout-icon.svg', route: '/login' }]" />
+        <!-- <AdminNotif [{profName="Prof Allan Knight"}, {className="CS 061"}, {roomNum="234"}]" /> -->
+        <AdminNotif profName='Knight' className='CS061' roomNum='234' />
+        <CornerStyle />
+    </div>
 </template>
