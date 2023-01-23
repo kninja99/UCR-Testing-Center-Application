f797a0295f2187597c0f97544d515a79e182c3fa
Kninja99 <kcdamschen@yahoo.com>
2023-01-22 20:36:21 -0800

    collapsable navbar
    * probably want to add an animation and adjust how the btn sticks but
      working

--- frontend/src/components/NavBtn.vue
+++ frontend/src/components/NavBtn.vue
@@ -1,7 +1,18 @@
 <template>
-    <div class="nav-btn">
+    <div v-on:click="navBarEvent()" class="nav-btn">
         <div class="nav-icon"></div>
         <div class="nav-icon"></div>
         <div class="nav-icon"></div>
     </div>
 </template>
+
+<script>
+    export default {
+        methods: {
+            navBarEvent() {
+                let navBar = document.querySelector(".nav-bar");
+                navBar.classList.toggle("nav-active")
+            }
+        },
+    }
+</script>
