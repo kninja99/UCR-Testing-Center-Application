026dd5349d455faaebec82a0b29fa0e0abcfd0e2
echu031 <echu031@ucr.edu>
2023-01-26 15:16:26 -0800

    Admin Scheduling room view init
    -branch creation on github
    -initial nav bar
    -edit button

---
+++ frontend/src/components/EditRoomAvailabilityButton.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    props: {
+        buttonName: String
+    }
+}   
+</script>
+
+<template>
+    <a href="#" class="edit-room-availability-button">
+        <span class="edit-text">{{buttonName}}</span>
+    </a>
+</template>
---
+++ frontend/src/views/AdminScheduling.vue
@@ -0,0 +1,39 @@
+<script>
+import Card from "../components/Card.vue"
+import NavBar from "../components/NavBar.vue"
+import CornerStyle from "../components/CornerStyle.vue"
+import Header from "../components/Header.vue";
+import EditRoomAvailabilityButton from "../components/EditRoomAvailabilityButton.vue"
+import EditRoomAvailabilityButton from "../components/EditRoomAvailabilityButton.vue";
+export default {    
+    components: {
+    Card,
+    NavBar,
+    Header,
+    CornerStyle,
+    EditRoomAvailabilityButton
+}
+}
+</script>
+
+
+
+<template>
+    <div class="admin-scheduling-home">
+        <Header title="Admin Scheduling"/>
+
+        <!-- nav-elements is an array of objects that map nav elements -->
+        <!-- object {title:Sting , img:String , route:String} -->
+        <NavBar :nav-elements="[
+        { title: 'Dashboard', img: 'dashBoard-icon.svg', route: '/admin' },
+        { title: 'Rooms', img: 'roomNav-icon.svg', route: 'admin/rooms' },
+        { title: 'Inbox', img: 'mailNav-icon.svg', route: 'admin/inbox' },
+        { title: 'Settings', img: 'settings-icon.svg', route: 'admin/settings' },
+        { title: 'My Account', img: 'profile-icon.svg', route: 'admin/account' },
+        { title: 'Logout', img: 'logout-icon.svg', route: '/login' }]" />
+        
+        <EditRoomAvailabilityButton buttonName="Edit Room Availability"/>
+        <CornerStyle />
+
+    </div>
+</template>
