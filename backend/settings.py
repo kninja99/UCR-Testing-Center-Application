52c6f489648cd25f000d0fa20e414e5f0db490b9
Kninja99 <kcdamschen@yahoo.com>
2023-01-26 20:45:17 -0800

    knox template for login
    * used template to see if I can get an authentication working
    * currently working to auth users that are registered with register link
      to api

--- backend/settings.py
+++ backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'knox'
 ]
 
 MIDDLEWARE = [
@@ -105,6 +106,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        #'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
+        'knox.auth.TokenAuthentication'
+    ]
+}
 
 
 # Internationalization
