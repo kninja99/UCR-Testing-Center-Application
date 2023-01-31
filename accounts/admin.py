a7281fa008f0864c710df47097c63af36efc7b30
Kninja99 <kcdamschen@yahoo.com>
2023-01-31 14:02:22 -0800

    Added extended user types
    * added simple extended users for our application
    * still working on setting everything up

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student, Professor, SiteAdmin, Proctor
 
-# Register your models here.
+admin.site.register(Student)
+admin.site.register(Professor)
+admin.site.register(SiteAdmin)
+admin.site.register(Proctor)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Custom User models
+
+class Student(models.Model):
+    student=models.OneToOneField(User,on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=50, blank = 'Student')
+    
+    def __str__(self):
+        return self.student.username
+    
+class SiteAdmin(models.Model):
+    site_admin=models.OneToOneField(User,on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=50, blank = 'Admin')
+    
+    def __str__(self):
+        return self.site_admin.username
+    
+class Professor(models.Model):
+    professor=models.OneToOneField(User,on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=50, blank = 'Professor')
+    
+    def __str__(self):
+        return self.professor.username
+    
+class Proctor(models.Model):
+    proctor=models.OneToOneField(User,on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=50, blank = 'Proctor')
+    
+    def __str__(self):
+        return self.proctor.username
