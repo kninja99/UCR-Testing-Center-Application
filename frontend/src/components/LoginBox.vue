73143e03c46eb32b09835161c3ab082a4bf962fb
Kninja99 <kcdamschen@yahoo.com>
2023-02-01 09:55:34 -0800

    post request to login
    * successful post request to our login api
    * user can now be authenticated
    * need to work on storing user data now

--- frontend/src/components/LoginBox.vue
+++ frontend/src/components/LoginBox.vue
@@ -2,7 +2,7 @@
     <div class="login-wrapper">
         <div class="login-box">
             <h2>Sign In</h2>
-            <form action="#">
+            <form @submit.prevent="loginEvent">
                 <input id = "username" type="text" placeholder="Username">
                 <input id = "password" type="password" placeholder="Password">
                 <button class = "login-btn">Login</button>
@@ -11,3 +11,29 @@
     </div>
     
 </template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        methods: {
+            loginEvent(e) {
+                // collecting our user data
+                let userName = document.querySelector("#username").value;
+                let pass = document.querySelector("#password").value;
+                let loginData = {
+                username: userName ,
+                password: pass
+                };
+                //authentication
+                axios.post('/api/login/', loginData)
+                .then((res) => {
+                    console.log(res);
+                })
+                .catch((err) => {
+                    console.log(err.response.data);
+                });
+                e.preventDefault(e);
+            }
+        },
+    }
+</script>
