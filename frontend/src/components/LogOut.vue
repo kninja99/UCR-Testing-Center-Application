0726cfa15822b1cccb25ca855bbfe1bbb64749c9
Kninja99 <kcdamschen@yahoo.com>
2023-02-09 08:46:04 -0800

    logout event
    * added a logout event associated with a click of the logout btn
    * clears the store and broswer data then routes back to login page

--- frontend/src/components/LogOut.vue
+++ frontend/src/components/LogOut.vue
@@ -1,5 +1,17 @@
+<script>
+    export default {
+        methods: {
+            logOutEvent() {
+                this.$store.commit('logout');
+                this.$router.push('/login');
+                console.log(this.$store.state);
+            }
+        }
+    }
+</script>
+
 <template>
-    <button class="nav-comp">
+    <button @click="logOutEvent" class="nav-comp">
         <img src="@/assets/imgs/logout-icon.svg" alt="logout">
         <h3>logout</h3>
     </button>
