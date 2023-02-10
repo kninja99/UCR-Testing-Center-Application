5f8876c3aa7e3b85a51d90364aac86e0edc180f9
Kninja99 <kcdamschen@yahoo.com>
2023-02-10 12:09:21 -0800

    Added account list to account
    * more info displayed under accounts in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Account
 
-admin.site.register(Account)
+#admin.site.register(Account)
+
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "user_type")
+    
+admin.site.register(Account, AccountAdmin)
