901a30e9a15275e91ac1a5e1ce7f62a57189690b
GregariousG <91446233+GregariousG@users.noreply.github.com>
2023-02-10 16:03:38 -0800

    a draft for how studentCheckinHeader should look

---
+++ frontend/src/components/StudentCheckInHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    props: {
+        status: String,
+        imgName: String,
+        route: String
+    }
+}   
+</script>
+
+<template>
+    <div class="status-header">
+        <router-link :to="route">
+            <button class="status">{{ status }}</button>
+        </router-link>
+        <h1 class="status">Status</h1>
+        <h2 class="name">Name</h2>
+        <h3 class="class">Class</h3>
+        <h4 class="professor">Professor</h4>
+        <h5 class="actions">Actions</h5>
+    </div>
+</template>
