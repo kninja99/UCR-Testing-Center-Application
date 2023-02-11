ec603aa221635d546da3986483bfe24b3245c802
GregariousG <91446233+GregariousG@users.noreply.github.com>
2023-02-10 16:06:38 -0800

    removed unnecessary code within studentCheckInHeader

--- frontend/src/components/StudentCheckInHeader.vue
+++ frontend/src/components/StudentCheckInHeader.vue
@@ -1,18 +1,5 @@
-<script>
-export default {
-    props: {
-        status: String,
-        imgName: String,
-        route: String
-    }
-}   
-</script>
-
 <template>
     <div class="status-header">
-        <router-link :to="route">
-            <button class="status">{{ status }}</button>
-        </router-link>
         <h1 class="status">Status</h1>
         <h2 class="name">Name</h2>
         <h3 class="class">Class</h3>
