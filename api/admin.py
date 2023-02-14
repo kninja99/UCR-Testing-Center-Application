8600330eabd334682b665ff923e0306a70ab5516
Kninja99 <kcdamschen@yahoo.com>
2023-02-14 15:31:00 -0800

    Room avalability basic model
    * probably will need to add to the model once we factor in professors
      but for now this will work

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from .models import Foo
-from .models import TestingRoom
+from .models import TestingRoom, TestingRoomAvailability
 
 admin.site.register(Foo)
 #admin.site.register(TestingRoom)
 
 class TestingRoomAdmin(admin.ModelAdmin):
     list_display = ("id", "room_number" , "bldg" , "capacity")
+    
+class TestingRoomAvailabilityAdmin(admin.ModelAdmin):
+    list_display = ("id", "testing_room" ,"start_time" , "end_time" , "date", "is_booked")
 
 admin.site.register(TestingRoom, TestingRoomAdmin)
+admin.site.register(TestingRoomAvailability,TestingRoomAvailabilityAdmin )
