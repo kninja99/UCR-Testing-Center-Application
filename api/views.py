54d42f954e12c5daff15024ef9042831974d1abc
Kninja99 <kcdamschen@yahoo.com>
2023-02-17 09:19:07 -0800

    added a way to get testing room data
    * added a temp get request end point to get testing room data
    * trying to figure out how to pass parameters to this request

--- api/views.py
+++ api/views.py
@@ -4,6 +4,8 @@ from rest_framework import viewsets
 
 from .serializers import FooSerializer, TestingRoomSerializer, TestingRoomAvailabilitySerializer
 from .models import Foo, TestingRoom, TestingRoomAvailability
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 def index(request):
     return HttpResponse("Hello, world. You're at the api index.")
@@ -16,6 +18,13 @@ class TestingRoomViewSet(viewsets.ModelViewSet):
     queryset = TestingRoom.objects.all()
     serializer_class = TestingRoomSerializer
     
+    @action(methods=['get'], detail=False)
+    def getTestingRoom(self,request):
+        getUserType = TestingRoom.objects.filter(bldg = "Winston Chung Hall").filter(room_number = 234)
+        serializer = TestingRoomSerializer(getUserType, many=True)
+        return Response(serializer.data)
+    
 class TestingRoomViewAvailabilitySet(viewsets.ModelViewSet):
+    #queryset = TestingRoomAvailability.objects.filter(testing_room_id = 7)
     queryset = TestingRoomAvailability.objects.all()
     serializer_class = TestingRoomAvailabilitySerializer
