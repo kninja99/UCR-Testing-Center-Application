cd7549d53e65910750bdce723d37a563fed75e64
Kninja99 <kcdamschen@yahoo.com>
2023-02-16 21:52:53 -0800

    added api endpoint to fetch all testing times

--- api/views.py
+++ api/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 from rest_framework import viewsets
 
-from .serializers import FooSerializer, TestingRoomSerializer
-from .models import Foo, TestingRoom
+from .serializers import FooSerializer, TestingRoomSerializer, TestingRoomAvailabilitySerializer
+from .models import Foo, TestingRoom, TestingRoomAvailability
 
 def index(request):
     return HttpResponse("Hello, world. You're at the api index.")
@@ -15,3 +15,7 @@ class FooViewSet(viewsets.ModelViewSet):
 class TestingRoomViewSet(viewsets.ModelViewSet):
     queryset = TestingRoom.objects.all()
     serializer_class = TestingRoomSerializer
+    
+class TestingRoomViewAvailabilitySet(viewsets.ModelViewSet):
+    queryset = TestingRoomAvailability.objects.all()
+    serializer_class = TestingRoomAvailabilitySerializer
