4c55e7217f7ef88ad557d98b9b2715ff9c0dd1c4
GregariousG <91446233+GregariousG@users.noreply.github.com>
2023-02-16 22:17:38 -0800

    checkin button created with green color and checkmark

---
+++ frontend/src/components/studentStatusButton.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    props: {
+        buttonName: String
+    }
+}   
+</script>
+
+<template>
+    <div href="#" class="student-status-button">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <span class="check-icon"><i class="fa fa-check"></i></span>
+        <span class="add-text">{{buttonName}}</span>
+    </div>
+</template>
