b081221b6a5760a1d26e7c9d535cec9c15a443c2
GregariousG <91446233+GregariousG@users.noreply.github.com>
2023-02-20 14:09:10 -0800

    finish styling touches

--- frontend/src/components/studentStatusButton.vue
+++ frontend/src/components/studentStatusButton.vue
@@ -6,23 +6,28 @@ export default {
         };
     },
     methods: {
-        statusChange() {
+        statusChange(e) {
             this.toggle = !this.toggle;
-            console.log("CLICKED");
+            let icon = e.target.querySelector(".fa")
+            if(!icon){
+                icon = e.target;
+            }
+            console.log(icon);
+            icon.classList.toggle("fa-check");
+            icon.classList.toggle("fa-close")
         },
     }
 }   
 </script>
 
 <template>
-    <button @click="statusChange()" 
+    <button @click="statusChange" 
         class="status-toggle"
         :style="{
         backgroundColor: toggle ? 'Green' : 'red'
       }">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <span class="check-icon"><i class="fa fa-check"></i></span>
-        <!--<span class="x-icon"><i class="fa fa-close"></i></span>-->
+        <span class="button-icon"><i class="fa fa-close"></i></span>
     </button>
     
 </template>
