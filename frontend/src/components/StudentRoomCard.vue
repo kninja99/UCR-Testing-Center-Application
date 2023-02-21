cafd6c81626c667793f4eb988ce6247cf3c82176
katdario <62965281+katdario@users.noreply.github.com>
2023-02-20 17:01:30 -0800

    created student room card component. Need fixes

---
+++ frontend/src/components/StudentRoomCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    props: {
+        // buttonHeader: String,
+        className: String,
+        profName: String
+    }
+}   
+</script>
+
+<template>
+    <div class="student-room-card-element">
+        <div class="student-room-card-element-color">
+        </div>
+
+        <div class="room-card-element-text">
+            <div class="room-card-element-class">
+                Course: {{className}}
+            </div>
+            <div class="room-card-element-seats">
+                Prof: {{profName}}
+            </div>
+        </div>
+    </div>
+</template>
