1f0d0d09baba938b47e5493614fdbf9226b3d07d
echu031 <echu031@ucr.edu>
2023-03-19 20:24:40 -0700

    Response when clicking admin inbox button
    Adding responses to the admin inbox buttons.

--- frontend/src/components/AdminNotifButtons.vue
+++ frontend/src/components/AdminNotifButtons.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="admin-notif-buttons">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <button class="check-button"><i class="fa fa-check"></i></button>
-        <button class="mail-button"><i class="fa fa-envelope"></i></button>
-        <button class="reject-button"><i class="fa fa-close"></i></button>
+        <button class='checkbutton'><i class="fa fa-check"></i></button>
+        <button class='mail-button'><i class="fa fa-envelope"></i></button>
+        <button class='reject-button'><i class="fa fa-close"></i></button>
     </div> 
 </template>
--- frontend/src/components/AdminNotifText.vue
+++ frontend/src/components/AdminNotifText.vue
@@ -4,6 +4,18 @@
         components: {
             AdminNotifButtons
         },
+        methods: {
+            checkbutton() {
+                alert("Reservation Accepted")
+            },
+            mailbutton() {
+                alert("Mailing")
+            },
+            rejectbutton() {
+                alert("Reservation Declined")
+            }
+        },
+    
         props: {
             profName: {
                 type:String,
@@ -48,9 +60,9 @@
         </div>
         <div class="admin-notif-buttons">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-            <button class="check-button"><i class="fa fa-check"></i></button>
-            <button class="mail-button"><i class="fa fa-envelope"></i></button>
-            <button class="reject-button"><i class="fa fa-close"></i></button>
+            <button class = "checkbutton" v-on:click="checkbutton"><i class="fa fa-check"></i></button>
+            <button class="mail-button" v-on:click="mailbutton"><i class="fa fa-envelope"></i></button>
+            <button class="reject-button" v-on:click="rejectbutton"><i class="fa fa-close"></i></button>
         </div> 
 
     </div> 
