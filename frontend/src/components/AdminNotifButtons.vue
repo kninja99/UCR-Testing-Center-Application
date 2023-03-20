86d100acfc4361ed808c79cf7fed42c5f9577c2a
echu031 <echu031@ucr.edu>
2023-03-19 21:11:42 -0700

    quickfix

--- frontend/src/components/AdminNotifButtons.vue
+++ frontend/src/components/AdminNotifButtons.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="admin-notif-buttons">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <button class='checkbutton'><i class="fa fa-check"></i></button>
+        <button class='check-button'><i class="fa fa-check"></i></button>
         <button class='mail-button'><i class="fa fa-envelope"></i></button>
         <button class='reject-button'><i class="fa fa-close"></i></button>
     </div> 
--- frontend/src/components/AdminNotifText.vue
+++ frontend/src/components/AdminNotifText.vue
@@ -68,7 +68,7 @@
         </div>
         <div class="admin-notif-buttons">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-            <button class = "checkbutton" v-on:click="checkbutton"><i class="fa fa-check"></i></button>
+            <button class = "check-button" v-on:click="checkbutton"><i class="fa fa-check"></i></button>
             <button class="mail-button" v-on:click="mailbutton"><i class="fa fa-envelope"></i></button>
             <button class="reject-button" v-on:click="rejectbutton"><i class="fa fa-close"></i></button>
         </div> 
