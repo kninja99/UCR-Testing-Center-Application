80d4bdfe65e99db3facca42de091ca46a4726c11
GregariousG <91446233+GregariousG@users.noreply.github.com>
2023-03-19 22:42:23 -0700

    added events for mail and flag buttons

--- frontend/src/components/studentStatusActions.vue
+++ frontend/src/components/studentStatusActions.vue
@@ -1,11 +1,23 @@
 <template>
     <div class ="action-buttons">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <button class="pmail-button">
+        <button v-on:click="mailbutton" class="pmail-button">
             <span class="mail-icon"><i class="fa fa-envelope"></i></span>
         </button>            
-        <button class="pflag-button">
+        <button v-on:click="flagbutton" class="pflag-button">
             <span class="mail-icon"><i class="fa fa-flag"></i></span>
         </button>
     </div>
 </template>
+<script>
+export default {
+    methods: {
+        mailbutton() {
+            alert("Sending Email")
+        },
+        flagbutton() {
+            alert("Student Flagged")
+        },
+    }
+}   
+</script>
